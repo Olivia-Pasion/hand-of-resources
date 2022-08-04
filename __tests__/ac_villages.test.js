7147651f0a2f1a8a5104316565ce1af68af0e5a2
olivia pasion <oap9820@gmail.com>
2022-08-04 11:23:11 -0700

    created #GET test for /ac_villagers route

--- __tests__/ac_villages.test.js
+++ __tests__/ac_villages.test.js
@@ -1,14 +1,22 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+//const { request } = require('express');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('#GET /ac_villagers should return a list of villagers', async () => {
+    const resp = await request(app).get('/ac_villagers');
+    expect(resp.body.length).toEqual(5);
+    expect(resp.body[0]).toEqual({
+      id: expect.any(String),
+      name: expect.any(String),
+      species: expect.any(String),
+      catchphrase: expect.any(String)
+    });
   });
   afterAll(async () => {
     await setup (pool);
