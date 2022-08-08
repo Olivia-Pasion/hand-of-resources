4fb34f122404886588465d1aa12c10e539b29c27
olivia pasion <oap9820@gmail.com>
2022-08-08 11:25:04 -0700

    #POST /movies passes

--- __tests__/movies.test.js
+++ __tests__/movies.test.js
@@ -23,6 +23,16 @@ describe('backend-express-template routes', () => {
       premiere: expect.any(String)
     });
   });
+  it('#POST /movies should add a new movie', async () => {
+    const resp = await request(app).post('/movies').send({ title: 'Pulp Fiction', director: 'Quentin Tarantino', premiere: '10-14-1994' });
+    expect(resp.status).toBe(200);
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      title: expect.any(String),
+      director: expect.any(String),
+      premiere: expect.any(String)
+    });
+  });
   afterAll(async () => {
     await setup (pool);
     pool.end();
