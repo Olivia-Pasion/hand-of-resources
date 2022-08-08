91a15be80486b48f0a60d31429368c73688fe580
olivia pasion <oap9820@gmail.com>
2022-08-08 14:18:34 -0700

    #GET /art_museums passes

---
+++ __tests__/art_museum.test.js
@@ -0,0 +1,22 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('#GET /art_museums should return a list of museums', async () => {
+    const resp = await request(app).get('/art_museums');
+    expect(resp.body.length).toEqual(5);
+    expect(resp.body[0]).toEqual({
+      id: expect.any(String),
+      name: expect.any(String)
+    });
+  });
+  afterAll(async () => {
+    await setup (pool);
+    pool.end();
+  });
+});
